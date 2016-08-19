80b3b496badaf7e57287280d071d0053e938f84f
Albert Chaharbakhshi <albeec13@gmail.com>
2016-08-19 01:28:13 -0500

    Add config file mechanism in preparation of Mysql integration

--- dashhandler.go
+++ dashhandler.go
@@ -4,7 +4,18 @@ import (
     "net/http"
     "github.com/gin-gonic/gin"
     "fmt"
+    "strings"
+    "errors"
+    "io/ioutil"
+    "encoding/json"
+//    "database/sql"
+//    _ "github.com/go-sql-driver/mysql"
 )
+
+type ConfigFile struct {
+    DBUser  string
+    DBPass  string
+}
 
 type DHCPEvent struct {
     Event    string
@@ -14,19 +25,44 @@ type DHCPEvent struct {
 }
 
 func main() {
+    var config ConfigFile
+    err := readConfigFile(&config)
     router := gin.Default()
 
+    fmt.Println(config)
+    fmt.Println(err)
+
     router.GET("/handle", func(c *gin.Context) {
-        dhcpevent := DHCPEvent{
+        dhcpevent := DHCPEvent {
             c.DefaultQuery("event", "null"),
-            c.DefaultQuery("mac", "null"),
+            strings.ToLower(c.DefaultQuery("mac", "null")),
             c.DefaultQuery("ip", "null"),
             c.DefaultQuery("hostname", "null"),
         }
         c.String(http.StatusOK, dhcpevent.Event)
-        fmt.Printf("%+v\n",dhcpevent)
+        fmt.Println(dhcpevent)
     })
 
     router.Run(":4469")
 }
 
+func readConfigFile(config *ConfigFile) (err error) {
+    file, err := ioutil.ReadFile("dashhandler.conf")
+    if file != nil {
+        return parseConfigFile(config, file)
+    } else {
+        return errors.New("Error: config file not found")
+    }
+}
+
+func parseConfigFile(config *ConfigFile, file []byte) (err error) {
+    if file != nil {
+        if err := json.Unmarshal(file, config); err == nil {
+            return nil
+        } else {
+            return errors.New("Error: invalid config JSON")
+        }
+    } else {
+        return errors.New("Error: config file not found")
+    }
+}
